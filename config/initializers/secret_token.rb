99f0f7a0778b1f0178737c9be0eb71b50ed45f2a
fuying <xianfuying@126.com>
2013-03-20 18:10:15 +0800

    init repo: Fu Ying

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datewithagent::Application.config.secret_token = '32fc9bd630f10b77c2e27280ab3a966480785e5b0160486ce26616cde8172d8e809cb684eb5ac32e491a8a1ba7f4f87ffab513cbd2a3a96c2846108f8dc672f6'
