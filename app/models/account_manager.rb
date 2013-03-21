e9a68256af14f407947b5f207b8b8973aba1de17
jfeng <jfeng@thoughtworks.com>
2013-03-21 09:46:59 +0800

    add account_manager has_many agents relation

--- app/models/account_manager.rb
+++ app/models/account_manager.rb
@@ -8,4 +8,6 @@ class AccountManager < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  has_many :agents
 end
