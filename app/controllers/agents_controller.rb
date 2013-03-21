76247e140ee106200f95a25b36cfb38ff57f9d2b
John <jwzhang@thoughtworks.com>
2013-03-21 13:36:18 +0800

    add get mood method

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -18,6 +18,22 @@ class AgentsController < ApplicationController
 
   def index
     @agents = Agent.where(:account_manager_id => current_account_manager.id)
+    @agents_map = generate_agents_mood(@agents)
+  end
+
+  private
+
+  def generate_agents_mood agents
+    agents_map = {}
+    agents.each do |agent|
+      events = Event.find_all_by_agent_id(agent.id)
+      mood_sum = 0
+      events.each do |event|
+        mood_sum += event.mode
+      end
+      agents_map[agent.name] = mood_sum
+    end
+    agents_map
   end
 
 end
--- lib/tasks/fake_agents.rake
+++ lib/tasks/fake_agents.rake
@@ -2,7 +2,10 @@ namespace :db do
   desc "Create fake agents"
   task fake_agents: :environment do
     10.times do |index|
-      Agent.create!(name: "AGENT#{index}")
+      agent = Agent.create!(name: "AGENT#{index}", account_manager_id: 1)
+      3.times do |time|
+        Event.create!(mode: rand(5) - rand(time), agent_id: agent.id)
+      end
     end
   end
 end
