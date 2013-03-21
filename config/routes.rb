1804aea0f9720896c44dd69d7e673f5f631dff9b
jfeng <jfeng@thoughtworks.com>
2013-03-21 08:43:02 +0800

    Create Account Manager Resourse

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Datewithagent::Application.routes.draw do
+  devise_for :account_managers
+
+  root :to => redirect("/account_managers/sign_in")
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,6 @@ Datewithagent::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
