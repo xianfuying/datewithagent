fff1c7e1b75bb0188c9a55106e3b2006a141cb5d
fuying <xianfuying@126.com>
2013-03-22 15:52:29 +0800

    use pretty time : Fu Ying

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,5 @@
 module EventsHelper
+  def pretty_date_format(date, format="%d/%b/%Y")
+    date.to_time > 1.week.ago ? distance_of_time_in_words(Time.now, date) + " ago" : date.strftime(format)
+  end
 end
